22c9922c667963b1ad454fe791a2e03c716a3448
A1Jhonas <carlos_5523@hotmail.com>
2025-01-22 13:29:56 -0500

    Portada

--- src/views/Home/Portada/portada.tsx
+++ src/views/Home/Portada/portada.tsx
@@ -1,32 +1,47 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faWhatsapp, faTiktok, faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons'
+
 import Logo from '../../../assets/svg/LOGO A COLOR.svg'
 function portada() {
   return (
     <div>
 
-      <div>
+      <div className='flex flex-1 '>
+
         <img  style={{width: "200px"}} src={Logo} alt="Logo" />
-        <p>Kanon</p>
-        <p>"Neque porro quisquam est qui dolorem 
-        ipsum quia dolor sit amet, consectetur, adipisci velit..."</p>
+
+        <div className='flex flex-col'>
+          <p className='text-[#563030] text-9xl font-bold'>Kanon</p>
+          <p className='text-[#FB8A70]'>"Neque porro quisquam est qui dolorem 
+          ipsum quia dolor sit amet, consectetur, adipisci velit..."</p>
+        </div>
+
       </div>
 
-      <div>
-        <p className='text-9xl'>Projecto 1</p>
-        <div></div>
+      <div className='flex text-center '>
+
+        <div className='flex flex-col mx-8'>
+          <p className='text-[#563030] text-3xl font-bold'>Projecto 1</p>
+          <div className='w-64 h-48 bg-[#FFD4A6] rounded-3xl'>Contenido</div>
+        </div>
+
+        <div className='flex flex-col mx-8'>
+          <div className='w-64 h-48 bg-[#FFD4A6] rounded-3xl'>Contenido</div>
+          <p className='text-[#563030] text-3xl font-bold'>Projecto 2</p>
+        </div>
+        
+        <div className='flex flex-col mx-8'>
+          <p className='text-[#563030] text-3xl font-bold'>Projecto 3</p>
+          <div className='w-64 h-48 bg-[#FFD4A6] rounded-3xl'>Contenido</div>
+        </div>
+
       </div>
 
-      <div>
-        <div></div>
-        <p>Projecto 2</p>
-      </div>
-      
-      <div>
-        <p>Projecto 3</p>
-        <div></div>
-      </div>
-
-      <div>
-
+      <div className='flex'>
+        <FontAwesomeIcon icon={faWhatsapp} />
+        <FontAwesomeIcon icon={faTiktok} />
+        <FontAwesomeIcon icon={faInstagram} />
+        <FontAwesomeIcon icon={faGithub} />
       </div>
 
     </div>
