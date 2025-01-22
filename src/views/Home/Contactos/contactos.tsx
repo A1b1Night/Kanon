a06374c323a34f85de1671609ef083de4b65df89
A1Jhonas <carlos_5523@hotmail.com>
2025-01-22 16:04:00 -0500

    responsive

--- src/views/Home/Contactos/contactos.tsx
+++ src/views/Home/Contactos/contactos.tsx
@@ -1,8 +1,81 @@
 function contactos() {
   return (
-    <div>
-      contactos
-    </div>
+    <form action="https://api.web3forms.com/submit" method="POST">
+        <input
+          type="hidden"
+          name="access_key"
+          // key del correo
+          value="850ffa62-7105-493e-a84b-9a0de1609fb6"
+        />
+        <input
+          type="hidden"
+          name="redirect"
+          value="http://localhost:5173/Aform"
+        />
+        <input
+          type="hidden"
+          name="subject"
+          value="Tienes una nueva notificación de KANON!!"
+        />
+        <div className="flex items-center w-screen mt-auto ">
+          <div className="container mx-auto my-3 px-4 lg:px-20 ">
+            <div className="w-full p-8 my-4 md:px-16 mr-auto rounded-2xl drop-shadow-2xl shadow-500">
+              <div className="flex">
+                <h1 className="font-bold text-[#563030] m-5 uppercase text-6xl">
+                  Contacto
+                </h1>
+              </div>
+              <div className="grid grid-cols-1 gap-5 md:grid-cols-2 mt-5">
+              <p className='text-[#563030] text-2xl font-bold'>Nombre</p>
+                <input
+                  className="w-full bg-[#FB8A70] text-[#563030] p-3 rounded-lg focus:outline-none"
+                  type="text"
+                  name="name"
+                  placeholder=""
+                />
+                <p className='text-[#563030] text-2xl font-bold'>Apellido</p>
+                <input
+                  className="w-full bg-[#FB8A70] text-[#563030] p-3 rounded-lg focus:outline-none"
+                  type="text"
+                  name="lastname"
+                  placeholder=""
+                />
+                <p className='text-[#563030] text-2xl font-bold'>E-mail</p>
+                <input
+                  className="w-full bg-[#FB8A70] text-[#563030] p-3 rounded-lg focus:outline-none"
+                  type="email"
+                  name="email"
+                  placeholder=""
+                />
+                <p className='text-[#563030] text-2xl font-bold'>Telefono</p>
+                <input
+                  className="w-full bg-[#FB8A70] text-[#563030] p-3 rounded-lg focus:outline-none"
+                  type="text"
+                  name="phone"
+                  placeholder=""
+                />
+              </div>
+              <div className="my-8">
+              <p className='text-[#563030] text-2xl font-bold'>Mensaje</p>
+                <textarea
+                  name="message"
+                  placeholder=""
+                  className="w-full bg-[#FB8A70] text-[#563030] p-3 rounded-lg focus:outline-none"
+                ></textarea>
+              </div>
+              <div className="my-2 w-1/2 lg:w-1/4">
+                <button
+                  type="submit"
+                  className="uppercase text-sm font-bold tracking-wide bg-[#FB8A70] text-[#563030] p-3 rounded-lg w-full 
+                    focus:outline-none focus:shadow-outline"
+                >
+                  Enviar
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
   )
 }
 
--- src/views/Home/Portada/portada.tsx
+++ src/views/Home/Portada/portada.tsx
@@ -1,47 +1,37 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faWhatsapp, faTiktok, faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 import Logo from '../../../assets/svg/LOGO A COLOR.svg'
 function portada() {
   return (
-    <div>
+    <div >
 
-      <div className='flex flex-1 '>
+      <div className='md:flex md:flex-1 mt-20'>
 
-        <img  style={{width: "200px"}} src={Logo} alt="Logo" />
+        <img style={{width: "18em", margin:"auto"}} src={Logo} alt="Logo" />
 
-        <div className='flex flex-col'>
-          <p className='text-[#563030] text-9xl font-bold'>Kanon</p>
-          <p className='text-[#FB8A70]'>"Neque porro quisquam est qui dolorem 
+        <div className='flex flex-col text-center md:text-left m-5 '>
+          <p className='text-[#563030] text-8xl md:text-9xl font-bold mt-7 md:mt-9'>KANON</p>
+          <p className='text-[#FB8A70] text-2xl'>"Neque porro quisquam est qui dolorem 
           ipsum quia dolor sit amet, consectetur, adipisci velit..."</p>
         </div>
 
       </div>
 
-      <div className='flex text-center '>
+      <div className='md:flex text-center mt-10'>
 
-        <div className='flex flex-col mx-8'>
+        <div className='flex flex-col md:mx-8'>
           <p className='text-[#563030] text-3xl font-bold'>Projecto 1</p>
-          <div className='w-64 h-48 bg-[#FFD4A6] rounded-3xl'>Contenido</div>
+          <div className='m-auto w-64 h-48 bg-[#FFD4A6] rounded-3xl'>Contenido</div>
         </div>
 
-        <div className='flex flex-col mx-8'>
-          <div className='w-64 h-48 bg-[#FFD4A6] rounded-3xl'>Contenido</div>
-          <p className='text-[#563030] text-3xl font-bold'>Projecto 2</p>
+        <div className='flex flex-col md:mx-8 mt-10'>
+        <p className='text-[#563030] text-3xl font-bold'>Projecto 2</p>
+          <div className='m-auto w-64 h-48 bg-[#FFD4A6] rounded-3xl'>Contenido</div>
         </div>
         
-        <div className='flex flex-col mx-8'>
+        <div className='flex flex-col md:mx-8 mt-10'>
           <p className='text-[#563030] text-3xl font-bold'>Projecto 3</p>
-          <div className='w-64 h-48 bg-[#FFD4A6] rounded-3xl'>Contenido</div>
+          <div className='m-auto w-64 h-48 bg-[#FFD4A6] rounded-3xl'>Contenido</div>
         </div>
-
-      </div>
-
-      <div className='flex'>
-        <FontAwesomeIcon icon={faWhatsapp} />
-        <FontAwesomeIcon icon={faTiktok} />
-        <FontAwesomeIcon icon={faInstagram} />
-        <FontAwesomeIcon icon={faGithub} />
       </div>
 
     </div>
